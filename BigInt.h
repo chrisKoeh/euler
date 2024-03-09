84fe04b6376df075e1cdc46db953541d2b07b5d2
Christian Koehler <c.koehler@ccv.eu>
2024-03-09 19:37:52 +0100

    stable euler80.cpp and BigInt (4.3s for N=100, P=10000)

---
+++ BigInt.h
@@ -0,0 +1,511 @@
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm> 
+
+namespace PositiveBigInt{
+    class BigInt
+    {
+        static constexpr unsigned long long threshold = 1e17;
+        public:
+            BigInt( unsigned long long n, int s_offset = -1, unsigned long long digit_count = 11000 )
+            : num(std::vector<unsigned long long>(digit_count / get_threshold_exp()))
+            , digit_count(digit_count)
+            , start_offset(s_offset != -1 ? s_offset : 0)
+            , end_offset(start_offset)
+            {
+                while( n / threshold > 0 )
+                {
+                    num[end_offset++] = n % threshold;
+                    n /= threshold;
+                }
+                num[end_offset++] = n;
+            }
+
+            BigInt operator*(unsigned long long factor)
+            {
+                BigInt target = *this;
+                target.multiply_factor(factor);
+                return target;
+            }
+
+            BigInt operator*(const BigInt& factor)
+            {
+                BigInt target = *this;
+                target *= factor;
+                return target;
+            }
+
+            void operator*=(const BigInt& factor)
+            {
+                BigInt original = *this;
+                *this = BigInt(0);
+                for( int i = factor.start_offset; i < factor.end_offset; i++ )
+                {
+                    BigInt cache = original;
+                    cache.multiply_factor(factor.num[i], i - factor.start_offset);
+                    *this += cache;
+                }
+            }
+
+            void operator*=(unsigned long long factor)
+            {
+                multiply_factor(factor);
+            }
+
+            BigInt operator+(const BigInt& summand)
+            {
+                BigInt target = *this;
+                target.add_big_int(summand);
+                return target;
+            }
+
+            BigInt operator+(unsigned long long summand)
+            {
+                BigInt target = *this;
+                unsigned long long keep = target.add_num_to_element_primitive(summand, start_offset);
+                finalize_keep(keep, start_offset+1);
+                return target;
+            }
+
+            void operator+=(const unsigned long long summand)
+            {
+                unsigned long long keep = add_num_to_element_primitive(summand, start_offset);
+                finalize_keep(keep, start_offset+1);
+            }
+
+            void operator+=(const BigInt& summand)
+            {
+                add_big_int(summand);
+            }
+
+            BigInt operator-(const BigInt& substract)
+            {
+                BigInt target = *this;
+                target.substract_big_int(substract);
+                return target;
+            }
+
+            void operator-=(const BigInt& substract)
+            {
+                substract_big_int(substract);
+            }
+
+            bool operator==(const BigInt& equal)
+            {
+                return num == equal.num;
+            }
+
+            bool operator>(const BigInt& smaller)
+            {
+                if( (end_offset - start_offset) != (smaller.end_offset - smaller.start_offset ) )
+                    return (end_offset - start_offset) > (smaller.end_offset - smaller.start_offset );
+
+                const int offset_diff = start_offset - smaller.start_offset;
+                for( int i = end_offset - 1; i >= start_offset; i-- )
+                {
+                    const auto num_i = num[i];
+                    const auto smaller_num_i = smaller.num[i - offset_diff];
+                    if(num_i != smaller_num_i) return num_i > smaller_num_i;
+                }
+                return false;
+            }
+
+            bool operator>=(const BigInt& smaller_equal)
+            {
+                return *this == smaller_equal || *this > smaller_equal;
+            }
+
+            bool operator<(const BigInt& larger)
+            {
+                return !(*this > larger) && !(*this == larger);
+            }
+
+            BigInt get_big_int_until(size_t reverse_index, size_t new_digit_count = 250) const
+            {
+                BigInt r(0, 0, new_digit_count);
+                int start_from = end_offset - 1 - reverse_index;
+                if( reverse_index > end_offset - 1 - start_offset ) start_from = start_offset;
+                r.end_offset = r.start_offset;
+                for(int i = start_from; i < end_offset; i++)
+                {
+                    r.num[i - start_from + r.start_offset] = num[i];
+                    r.end_offset++;
+                }
+                return r;
+            }
+
+            int get_digit_sum(int until = 0)
+            {
+                int count_digits = 0;
+                int digit_sum = 0;
+                const int thres_exp = get_threshold_exp();
+                for( int i = end_offset - 1; i >= start_offset; i-- )
+                {
+                    std::string s = std::to_string(num[i]);
+                    while( s.length() < thres_exp && i != end_offset - 1 ) s = "0" + s;
+                    for( int i = 0; i < s.length(); i++ )
+                    {
+                        if( until != 0 && count_digits >= until ) return digit_sum;
+                        count_digits++;
+                        digit_sum += s[i] - 48;
+                    }
+                }
+                return digit_sum;
+            }
+
+            int get_digit_count()
+            {
+                int count_digits = 0;
+                const int thres_exp = get_threshold_exp();
+                if( end_offset - start_offset > 1 ) count_digits = (thres_exp * (end_offset - 1 - start_offset));
+                count_digits += std::to_string(num[end_offset - 1]).length();
+                return count_digits;
+            }
+
+            friend std::ostream& operator<<(std::ostream& os, const BigInt& bi)
+            {
+                os << bi.get_as_string();
+                return os;
+            }
+
+            std::string get_as_string() const
+            {
+                std::stringstream os;
+                if( end_offset - start_offset >= 1 )
+                {
+                    os << num[end_offset - 1];
+                }
+
+                for( int i = end_offset - 2; i >= start_offset; i-- ) os << std::setfill('0') << std::setw(get_threshold_exp()) << num[i];
+                return os.str();
+            }
+
+            std::string get_as_string_reversed() const
+            {
+                std::stringstream os;
+                for( int i = start_offset; i < end_offset - 1; i++ )
+                {
+                    std::stringstream os_;
+                    os_ << std::setfill('0') << std::setw(get_threshold_exp()) << num[i];
+                    std::string s(os_.str());
+                    std::reverse(s.begin(), s.end());
+                    os << s;
+                }
+                auto s = std::to_string(num[end_offset - 1]);
+                std::reverse(s.begin(), s.end());
+                os << s;
+                return os.str();
+            }
+
+            std::vector<unsigned long long> num;
+            size_t digit_count;
+            int start_offset;
+            int end_offset;
+
+            unsigned long long get_threshold_exp() const
+                {
+                    unsigned long long threshold_exp = 1;
+                    unsigned long long threshold_cache = threshold;
+                    while( (unsigned long long)threshold_cache / (unsigned long long)10 > 1 )
+                    {
+                        threshold_exp++;
+                        threshold_cache /= (unsigned long long)10;
+                    }
+                    return threshold_exp;
+                }
+
+            private:
+                unsigned long long add_num_to_element_primitive(const unsigned long long& n, const int index)
+                {
+                    unsigned long long& numi = num[index];
+                    numi += n;
+                    const unsigned long long res = numi / threshold;
+                    numi %= threshold;
+                    return res;
+                }
+
+                void finalize_keep(unsigned long long& keep, int start_from)
+                {
+                    while( keep != 0 )
+                    {
+                        keep = add_num_to_element_primitive(keep, start_from);
+                        if( start_from++ >= end_offset ) end_offset++;
+                    }
+                }
+
+                void add_big_int(const BigInt& bb)
+                {
+                    auto& a = num;
+                    const auto& b = bb.num;
+                    const int b_size = bb.end_offset;
+                    unsigned long long keep = 0;
+                    for( int i = bb.start_offset; i < b_size; i++ )
+                    {
+                        const int aind = i + start_offset - bb.start_offset;
+                        const unsigned long long bi = b[i];
+                        keep = add_num_to_element_primitive(bi + keep, aind);
+                        if( aind >= end_offset )
+                        {
+                            end_offset++;
+                        }
+                    }
+                    finalize_keep(keep, b_size + start_offset - bb.start_offset);
+                }
+
+                void substract_big_int(const BigInt& bb)
+                {
+                    auto& a = num;
+                    const auto& b = bb.num;
+                    const int b_size = bb.end_offset;
+                    uint8_t keep = 0;
+                    const int start_offset_diff = start_offset - bb.start_offset;
+                    for( int i = bb.start_offset; i < b_size; i++ )
+                    {
+                        unsigned long long* ai = &a[i + start_offset_diff];
+                        long long c = (long long)*ai - b[i] - keep;
+                        if( c < 0 )
+                        {
+                            *ai = c + threshold;
+                            keep = 1;
+                        }
+                        else
+                        {
+                            *ai = c;
+                            keep = 0;
+                        }
+                    }
+                    unsigned long long * ai = &a[b_size + start_offset_diff];
+                    while( keep == 1 )
+                    {
+                        long long c = (long long)*ai - 1;
+                        if( c < 0 )
+                        {
+                            *ai = c + threshold;
+                            ai++;
+                        }
+                        else
+                        {
+                            keep = 0;
+                            *ai = c;
+                        }
+                    }
+                    while( a[end_offset - 1] == 0 && end_offset - start_offset > 1 ) end_offset--;
+                }
+
+                void multiply_factor(const unsigned long long& factor, const int offset = 0)
+                {
+                    auto& a = num;
+                    if( factor == 0 )
+                    {
+                        end_offset = start_offset;
+                        a[end_offset++] = 0;
+                        return;
+                    }
+
+                    if( factor == 1 && offset == 0 ) return;
+
+                    if( offset != 0 )
+                    {
+                        BigInt cache(0);
+                        cache.end_offset = offset + end_offset;
+                        cache.start_offset = start_offset;
+                        unsigned long long keep = 0;
+                        for( int i = start_offset; i < end_offset; i++ )
+                        {
+                            const unsigned long c = (num[i] * factor) + keep;
+                            keep = cache.add_num_to_element_primitive( c, i + offset);
+                        }
+                        cache.finalize_keep( keep, cache.end_offset);
+                        *this = cache;
+                    }
+                    else
+                    {
+                        end_offset += offset;
+                        unsigned long long keep = 0;
+                        for( int i = start_offset; i < end_offset; i++ )
+                        {
+                            const unsigned long c = (num[i] * (factor - 1)) + keep;
+                            keep = add_num_to_element_primitive( c, i + offset);
+                        }
+                        finalize_keep( keep, end_offset);
+                    }
+                }
+    };
+
+    void unit_test(BigInt a, const std::string& expected )
+    {
+        std::string printed_a = a.get_as_string();
+        const int width = 60;
+        if( printed_a.size() > 50 )
+        {
+            printed_a = std::string(printed_a.begin(), printed_a.begin() + width - 21);
+            printed_a += "...(digits=" + std::to_string(a.get_digit_count()) + ")";
+        }
+        std::stringstream ss;
+        ss << printed_a;
+        std::cout << "TEST a=" << printed_a << " " << std::setfill(' ') << std::setw(width - ss.str().length());
+        if(a.get_as_string() == expected ) std::cout << "PASS" << std::endl;
+        else std::cout << "FAIL" << std::endl;
+    }
+
+    void unit_test_operator(bool result, bool expected )
+    {
+        std::cout << "TEST operator " << std::setfill(' ') << std::setw(54);
+        if(result == expected ) std::cout << "PASS" << std::endl;
+        else std::cout  << "FAIL" << std::endl;
+    }
+
+    void unit_tests()
+    {
+        BigInt a(0);
+        BigInt b(10);
+
+        unit_test(a, "0");
+        a = a+b;
+        unit_test(a, "10");
+        a = a * BigInt(1000);
+        unit_test(a, "10000");
+        a = a - b;
+        unit_test(a, "9990");
+        a = a * 8;
+        unit_test(a, "79920");
+        BigInt c(79900);
+        a = a - c;
+        unit_test(a, "20");
+        a = a * 7777;
+        unit_test(a, "155540");
+        a = a * 6666;
+        unit_test(a, "1036829640");
+        BigInt d(1036829640);
+        a = a - d;
+        unit_test(a, "0");
+        a = a + d;
+        unit_test(a, "1036829640");
+        BigInt e(9640);
+        a = a - e;
+        unit_test(a, "1036820000");
+        BigInt f(5);
+        a = a - f;
+        unit_test(a, "1036819995");
+        a += 0;
+        unit_test(a, "1036819995");
+        a -= 0;
+        unit_test(a, "1036819995");
+        f += 100000;
+        unit_test(f, "100005");
+        f += e;
+        unit_test(f, "109645");
+        f -= e;
+        unit_test(f, "100005");
+        f *= 10;
+        unit_test(f, "1000050");
+        f *= 20;
+        unit_test(f, "20001000");
+
+        BigInt o = f;
+        unit_test_operator(o > f, false);
+        unit_test_operator(o == f, true);
+        o += 1;
+        unit_test_operator(o > f, true);
+        unit_test_operator(o < f, false);
+        unit_test_operator(a > f, true);
+        unit_test_operator(a > o, true);
+        o -= 1;
+        unit_test_operator(o == f, true);
+        unit_test_operator(o < f, false);
+        unit_test_operator(o > f, false);
+        unit_test_operator(f > o, false);
+
+        a *= 0;
+        unit_test(a, "0");
+        BigInt m(9999);
+        BigInt n(9999);
+        m *= n;
+        unit_test(m, "99980001");
+        m *= n;
+        unit_test(m, "999700029999");
+        BigInt s(9900);
+        BigInt t(9901);
+        s*=t;
+        unit_test(s, "98019900");
+        BigInt u(999099);
+        BigInt v(99901);
+        u *= v;
+        unit_test(u, "99810989199");
+        BigInt from(9999999);
+        BigInt split_2 = from.get_big_int_until(2);
+        unit_test(split_2, "99999");
+        BigInt split_4 = from.get_big_int_until(4);
+        unit_test(split_4, "9999999");
+        BigInt split_5 = from.get_big_int_until(5);
+        unit_test(split_5, "9999999");
+        BigInt split_10 = from.get_big_int_until(10);
+        unit_test(split_10,"9999999");
+
+        BigInt a1(99009900);
+        BigInt a2(990099);
+        a1 *= a2;
+        unit_test(a1, "98029602980100");
+        a1 = BigInt(99009900);
+        a2 = BigInt(990099);
+        a2 *= a1;
+        unit_test(a2, "98029602980100");
+        BigInt mult(990100);
+        BigInt fact(990100);
+        mult *= fact;
+        unit_test(mult, "980298010000");
+        unit_test_operator(mult.get_digit_count() == 12, true);
+        unit_test_operator(mult.get_digit_sum(3) == 17, true);
+        unit_test_operator(mult.get_digit_sum(5) == 28, true);
+        unit_test_operator(mult.get_digit_sum(6) == 36, true);
+        unit_test_operator(mult.get_digit_sum(9) == 37, true);
+        BigInt a_off(1);
+        BigInt a_off11(0);
+        a_off11.start_offset = 11;
+        a_off11.end_offset = 12;
+        a_off11 += 5;
+        unit_test(a_off11, "5");
+        a_off += a_off11;
+        unit_test(a_off, "6");
+        a_off -= 4;
+        unit_test(a_off, "2");
+        a_off11 -= 3;
+        unit_test(a_off11, "2");
+        a_off11 = a_off11 + a_off;
+        unit_test(a_off11, "4");
+       a_off11 *= a_off;
+        unit_test(a_off11, "8");
+        a_off11 *= 87;
+        unit_test(a_off11, "696");
+        a_off *= 87;
+        unit_test(a_off, "174");
+        a_off11 *= a_off;
+        unit_test(a_off11, "121104");
+        a_off *= 100;
+        unit_test(a_off, "17400");
+
+        // performance tests
+        BigInt perf(1);
+        std::string s_perf = "1";
+        for( int i = 0; i < 10000; i++ )
+        {
+            perf *= 10;
+            s_perf.push_back(48);
+        }
+        unit_test(perf, s_perf);
+
+        perf = BigInt(1);
+        s_perf = "1";
+        srand((unsigned) time(NULL));
+        for( int i = 0; i < 1000; i++ )
+        {
+            int random = rand() % 10;
+            perf *= 10;
+            perf += random;
+            s_perf.push_back( random + 48 );
+        }
+        unit_test( perf, s_perf );
+   }
+}
---
+++ euler80.cpp
@@ -0,0 +1,203 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <chrono>
+
+#include "BigInt.h"
+
+using namespace PositiveBigInt;
+using namespace std::chrono;
+
+#define PRIME_MAX 1000
+
+std::map<int, BigInt> solutions;
+std::vector<int> primes = {2,3,5,7};
+std::vector<int> squares = {};
+
+bool is_prime(long long num) {
+    if(num < 2) return false;
+    for(const auto &val:primes) {
+        if(val > num / val) return true;
+        if(num % val == 0) return false;
+    }
+    return true;
+}
+
+void find_primes_to_n() {
+    for(int i = 9; i < PRIME_MAX; i+=2) {
+        if(is_prime(i)) primes.push_back(i);
+    }
+}
+
+void find_squares_to_n(int N)
+{
+    for(int i = 2; i <= N; i++)
+    {
+        const int sq = i*i;
+        squares.push_back( sq );
+        if(sq > N) break;
+    }
+}
+
+bool is_perfect( int n )
+{
+    int sq_rt = sqrt(n);
+    return sq_rt * sq_rt == n;
+}
+
+BigInt* get_value_from_solutions(int num)
+{
+    auto pos = solutions.find(num);
+    if (pos != solutions.end()) {
+        return &pos->second;
+    }
+    return nullptr;
+}
+
+std::pair<int,int> get_square_factor( int n )
+{
+    std::pair<int,int> res = std::make_pair(-1,-1);
+    for( const auto& sq: squares )
+    {
+        const int div_cache = n / sq;
+        if( div_cache * sq == n )
+        {
+            res = std::make_pair(div_cache, sqrt(sq));
+        }
+        if( div_cache <= 1 ) break;
+    }
+    return res;
+}
+
+void sqrt_new(const int target, const int P)
+{
+    int start_num = sqrt( target );
+    BigInt start = BigInt(start_num*start_num);
+    BigInt target_cache(target);
+
+    int old_num = start_num;
+
+    BigInt estimate_factor(0, 10500 / start.get_threshold_exp());
+    BigInt est_factor_cache(old_num,0,50);
+    BigInt estimate_factor_cmpl(0);
+    uint8_t count_zero = 0;
+    int realP = start_num > 9 ? P - 2: P - 1;
+    int init_duration = 0;
+    for(int i = 0; i < realP; i++)
+    {
+        // slow, 40ms for P=10000, 15ms for substraction, 9ms for *100, 8ms for *10
+        //auto c_start = high_resolution_clock::now();
+        target_cache -= start;
+        target_cache *= 100;
+        //auto c_stop = high_resolution_clock::now();
+        //auto duration = duration_cast<microseconds>(c_stop - c_start);
+        //init_duration += duration.count();
+        estimate_factor_cmpl *= 10;
+        estimate_factor_cmpl += (int)(old_num*20);
+
+        int estimate_num(0);
+
+        if( target_cache.get_digit_count() < 50 )
+        {
+            start = ( estimate_factor_cmpl + estimate_num) * estimate_num;
+            while( start < target_cache )
+            {
+                estimate_num++;
+                start = ( estimate_factor_cmpl + estimate_num) * estimate_num;
+            }
+            if( estimate_num > 0 )
+            {
+                estimate_num--;
+                start = ( estimate_factor_cmpl + estimate_num) * estimate_num;
+            }
+        }
+        else
+        {
+            BigInt rough_estimate = estimate_factor_cmpl.get_big_int_until(5);
+            BigInt rough_target = target_cache.get_big_int_until(5);
+            const int diff_digit_count = target_cache.get_digit_count() - estimate_factor_cmpl.get_digit_count();
+            int rough_diff_digit_count = rough_target.get_digit_count() - rough_estimate.get_digit_count();
+            while( diff_digit_count < rough_diff_digit_count )
+            {
+                rough_estimate *= 10;
+                rough_diff_digit_count--;
+            }
+            while( diff_digit_count > rough_diff_digit_count )
+            {
+                rough_target *= 10;
+                rough_diff_digit_count++;
+            }
+            BigInt estimated = rough_estimate;
+            estimated *= estimate_num;
+            while( estimated < rough_target )
+            {
+                estimate_num++;
+                estimated = rough_estimate;
+                estimated *= estimate_num;
+            }
+
+            // 7ms in total for P=10000
+            estimate_num--;
+            start = estimate_factor_cmpl;
+            start += estimate_num;
+            start *= estimate_num;
+        }
+
+        // fast, 0.1ms in total for P=10000
+        old_num = estimate_num;
+        est_factor_cache *= 10;
+        est_factor_cache += estimate_num;
+        if( est_factor_cache.num[est_factor_cache.start_offset] == 0 ) count_zero++;
+
+        if( est_factor_cache.get_digit_count() + count_zero == est_factor_cache.get_threshold_exp() )
+        {
+            estimate_factor.start_offset--;
+            estimate_factor += est_factor_cache;
+            est_factor_cache = BigInt(0,0,50);
+            count_zero = 0;
+        }
+    }
+
+    // fast
+    const int total_digits = est_factor_cache.get_digit_count() + count_zero;
+    for( int i = 0; i < total_digits/2; i++ ) estimate_factor *= 100;
+    if( total_digits % 2 == 1 ) estimate_factor *= 10;
+    estimate_factor += est_factor_cache;
+    solutions.insert( {target, estimate_factor});
+    //std::cerr << target << " init took " << init_duration << "mus" << std::endl;
+}
+
+int main()
+{
+    int N = 100;
+    int P = 10000;
+    find_squares_to_n(N);
+    find_primes_to_n();
+    unsigned long long total_sum = 0;
+    for( int i = 2; i <= N; i++ )
+    {
+        // break;
+        if( is_perfect( i ) ) continue;
+        const auto sqf = get_square_factor(i);
+        if( sqf.first != -1 )
+        {
+            auto* val = get_value_from_solutions(sqf.first);
+            BigInt sqrtX = *val;
+            sqrtX *= sqf.second;
+            total_sum += sqrtX.get_digit_sum(P);
+        }
+        else
+        {
+            sqrt_new(i, P+5);
+
+            // 4ms in total
+            total_sum += get_value_from_solutions(i)->get_digit_sum(P);
+        }
+    }
+    std::cout << total_sum << std::endl;
+
+    // unit_tests();
+    return 0;
+}
